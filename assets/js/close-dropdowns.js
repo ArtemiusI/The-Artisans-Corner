3b825162840183b18426c442270d20d476fd10ed
ArtemiusI <whuangx10@gmail.com>
2026-01-21 03:09:55 +0800

    .

--- assets/js/close-dropdowns.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    // Handle clicks on links with the 'close-dropdown' class
-    $('.close-dropdown').on('click', function(event) {
-        // Find the parent dropdown menu and hide it
-        $(this).closest('.dropdown-menu').removeClass('show');
-        // Find the dropdown toggle button and update its aria-expanded attribute
-        $(this).closest('.dropdown').find('.dropdown-toggle').attr('aria-expanded', false);
-    });
-});
