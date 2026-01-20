cf5924d0044e47db13240fc87037f3f6df1eaff0
ArtemiusI <whuangx10@gmail.com>
2026-01-21 03:41:51 +0800

    .

--- assets/js/custom-script.js
+++
@@ -1,4 +0,0 @@
-// Example: Prevent dropdown from closing when clicking a specific 'keep-open' div
-document.querySelector('.dropdown-menu .keep-open').addEventListener('click', function(e) {
-  e.stopPropagation(); // Stops the dropdown from closing
-});
